6dd36a9725149b5e158c82a82b878a1c41727836
cover-0 <son5kimth@hs.ac.kr>
2026-05-24 11:47:27 +0900

    fix: Vercel 배포 빌드 에러 수정 (불필요한 주석 제거)

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -73,7 +73,6 @@ export default async function PostsPage({ searchParams }: PostsPageProps) {
                 >
                   <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
                 </svg>
-                {/* @ts-expect-error: post_likes nested count type isn't fully inferred by default */}
                 <span>{post.post_likes?.[0]?.count || 0}</span>
               </div>
             </div>
